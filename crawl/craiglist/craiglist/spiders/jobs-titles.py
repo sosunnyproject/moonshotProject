cdb26ac3c3e0af581ab1627dc7ecec42e11d9aeb
SoSun <parksosun1103@gmail.com>
2017-10-03 22:50:21 +0900

    initial draft

---
+++ crawl/craiglist/craiglist/spiders/jobs-titles.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    allowed_domains = ['https://newyork.craigslist.org/search/egr']
+    start_urls = ['https://newyork.craigslist.org/search/egr/']
+
+    def parse(self, response):
+        titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        for title in titles:
+            yield {'Title': title}
+
